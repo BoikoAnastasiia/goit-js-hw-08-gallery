5690ffd31aea8cbd9e521b17e2401a2a1d1b0317
anastasyah86 <nastasyah86@gmail.com>
2020-12-26 17:46:55 +0200

    грусть(

--- js/gallery-create.js
+++ js/gallery-create.js
@@ -1,77 +1,74 @@
 import images from "./gallery-items.js";
-const lightboxImage = document.querySelector("lightbox__image");
-const lightboxOverlay = document.querySelector("lightbox__overlay");
+
 const ulRef = document.querySelector(".js-gallery");
 const modalRef = document.querySelector(".js-lightbox");
+const lightboxImage = modalRef.querySelector(".lightbox__image");
 
-const createGallery = (image) => {
+const galleryCreate = (elem) => {
   const liRef = document.createElement("li");
   liRef.classList.add("gallery__item");
-
   const linkRef = document.createElement("a");
   linkRef.classList.add("gallery__link");
-  linkRef.setAttribute("href", image.original);
-
+  linkRef.setAttribute("href", elem.original);
   const imgRef = document.createElement("img");
   imgRef.classList.add("gallery__image");
-  imgRef.setAttribute("src", image.preview);
-  imgRef.setAttribute("data-source", image.original);
-  imgRef.setAttribute("alt", image.description);
-
+  imgRef.setAttribute("src", elem.preview);
+  imgRef.setAttribute("data-set", elem.original);
+  imgRef.setAttribute("alt", elem.description);
   linkRef.appendChild(imgRef);
   liRef.appendChild(linkRef);
 
   return liRef;
 };
 
-const galleryMap = images.map(createGallery);
-// console.log(galleryMap);
-
-function addModal() {
-  lightboxImage.setAttribute("src", event.target.getAttribute("data-set"));
-  lightboxImage.setAttribute("alt", event.target.getAttribute("alt"));
-  modalRef.classList.add("is-open");
-  modalRef.addEventListener("click", closeModalHandler);
-  window.addEventListener("keydown", SwitchHandler);
-}
+const galleryItems = images.map(galleryCreate);
 
 function removeModal() {
   modalRef.classList.remove("is-open");
   lightboxImage.setAttribute("src", "");
   lightboxImage.setAttribute("alt", "");
   lightboxImage.innerHTML = "";
-  window.removeEventListener("keydown", SwitchHandler);
-  modalRef.removeEventListener("click", closeModalHandler);
+  window.removeEventListener("keydown", switchHandler);
+  modalRef.removeEventListener("click", closeModal);
 }
-const SwitchHandler = (event) => {
+
+function addModal() {
+  lightboxImage.setAttribute("src", event.target.getAttribute("data-set"));
+  lightboxImage.setAttribute("alt", event.target.getAttribute("alt"));
+  modalRef.classList.add("is-open");
+  modalRef.addEventListener("click", closeModal);
+  window.addEventListener("keydown", switchHandler);
+}
+
+const switchHandler = (event) => {
+  //   console.dir(event.code);
   if (event.code === "Escape") {
     removeModal();
   }
-
-  const galleryLinks = images.map(({ original }) => original);
+  const imagesLinks = images.map(({ original }) => original);
   const prevImg = lightboxImage.getAttribute("src");
-
+  // console.dir(imagesLinks);
   if (event.code === "ArrowLeft") {
-    if (galleryLinks.indexOf(prevImg) > 0) {
+    if (imagesLinks.indexOf(prevImg) > 0) {
       lightboxImage.setAttribute(
         "src",
-        galleryLinks[galleryLinks.indexOf(prevImg) - 1]
+        imagesLinks[imagesLinks.indexOf(prevImg) - 1]
       );
       lightboxImage.setAttribute(
         "alt",
-        images[galleryLinks.indexOf(prevImg) - 1].description
+        images[imagesLinks.indexOf(prevImg) - 1].description
       );
     }
   }
   if (event.code === "ArrowRight") {
-    if (galleryLinks.indexOf(prevImgSrc) < galleryLinks.length - 1) {
+    if (imagesLinks.indexOf(prevImg) < imagesLinks.length - 1) {
       lightboxImage.setAttribute(
         "src",
-        galleryLinks[galleryLinks.indexOf(prevImgSrc) + 1]
+        imagesLinks[imagesLinks.indexOf(prevImg) + 1]
       );
       lightboxImage.setAttribute(
         "alt",
-        galleryArray[galleryLinks.indexOf(prevImgSrc) + 1].description
+        images[imagesLinks.indexOf(prevImg) + 1].description
       );
     }
   }
@@ -90,7 +87,10 @@ const openModal = (event) => {
     addModal();
     event.preventDefault();
   }
+  //   getImgUrl(event);
+  //   console.log(getImgUrl(event));
 };
-ulRef.append(...galleryMap);
+ulRef.append(...galleryItems);
+// console.dir(thumbGallery);
 
 ulRef.addEventListener("click", openModal);
